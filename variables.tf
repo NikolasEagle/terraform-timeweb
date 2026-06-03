c3bd6c692854a808d49d60a3018692fe9d14a0e0
Nickolas Eagle <eagle.dev.stack@gmail.com>
2026-06-03 15:44:52 +0400

    Определение переменных для ip адресов серверов

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "twc_token" {
   type        = string
   sensitive   = true
 }
+
+variable "ipv4_kz" {
+    description = "IP VDS KZ"
+    type = string
+    sensitive = true
+}
+
+variable "ipv4_ru" {
+    description = "IP VDS RU"
+    type = string
+    sensitive = true
+}
